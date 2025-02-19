bb64cbc4080f0888833eb3598b3c381b81f0fe29
tgbro <tgbro@BROWNINGS2K24>
2025-02-19 14:21:49 -0500

    Add project files.

---
+++ MyConsoleDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Hello, World!");
+//Console.Write("Hello, World!");
+
+Console.Write("What is your first name: ");
+string? firstName = Console.ReadLine();
+
+Console.WriteLine($"Your first name is { firstName }");
+
+Console.ReadLine();
